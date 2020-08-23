2e82c725ec26c5d6ff8e6735facb86f5dacf6cad
Buddy Lodder <b.lodder@bl-ict.nl>
2020-08-23 21:06:07 +0200

    eerste opdrachtjes boek mbt. lists

---
+++ Crash course/chapter3/changing_adding_removing_elements.py
@@ -0,0 +1,45 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
+
+print("\n")
+print('-' * 40)
+motorcycles = []
+print(motorcycles)
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+print("\n")
+print('-' * 40)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(1, 'ducati')
+print(motorcycles)
+
+del motorcycles[1]
+print(motorcycles)
+
+print("\n")
+print('-' * 40)
+motorcycles = ['honda', 'ducati', 'yamaha', 'suzuki']
+print(motorcycles)
+
+print("\n")
+print('-' * 40)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+print("\n")
+print('-' * 40)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print(f"The first motorocycle I owned was a {first_owned.title()}.")

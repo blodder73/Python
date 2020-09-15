4c1a93f9bcc17e1482450c70e50ecf44403b0d45
Buddy Lodder <b.lodder@bl-ict.nl>
2020-09-15 17:58:13 +0200

    Tuples introduction:
    tuples_intro.py
    unpacking.py

---
+++ Python Masterclass/Sequences/tuples_intro.py
@@ -0,0 +1,26 @@
+t = ("a", "b", "c")
+print(t)
+
+name = "Tim"
+age = 10
+print("\n")
+print(name, age, "Python", 2020)
+print((name, age, "Python", 2020))
+
+print("\n")
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Copmany", "Bad Copmany", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+metallica2 = list(metallica)
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)

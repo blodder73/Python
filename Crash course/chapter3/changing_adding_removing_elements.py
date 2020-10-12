8665740be1821938bda98bba4fa0d78e0c976008
Buddy Lodder <b.lodder@bl-ict.nl>
2020-10-12 19:40:23 +0200

    some more lists

--- Crash course/chapter3/changing_adding_removing_elements.py
+++ Crash course/chapter3/changing_adding_removing_elements.py
@@ -43,3 +43,17 @@ print('-' * 40)
 motorcycles = ['honda', 'yamaha', 'suzuki']
 first_owned = motorcycles.pop(0)
 print(f"The first motorocycle I owned was a {first_owned.title()}.")
+
+
+print("\nRemove by value name")
+print('-' * 40)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")

97614d958ec680933369b40039c1743e3f844e40
Buddy Lodder <b.lodder@bl-ict.nl>
2020-10-15 17:53:21 +0200

    dictionaries.py
    - Nesting_dictionaries.py

--- Crash course/Chapter6/Nesting_dictionaries.py
+++ Crash course/Chapter6/Nesting_dictionaries.py
@@ -4,3 +4,54 @@ aliens = []
 # make 30 green aliens.
 for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+# Show the first 5 aliens.
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# Show how many aliens have been created.
+print(f"Total number of aliens: {len(aliens)}")
+
+
+# A list in a Dictionary
+
+# Store information about a pizza being ordered
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+# Summarize the order
+print(f"\n\nYou ordered a {pizza['crust']}-crust pizza "
+      "with the following toppings:")
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
+
+
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+}
+
+print("\n\n")
+for name, languages in favorite_languages.items():
+    if len(languages) > 1:
+        print(f"\n{name.title()}'s favorite languages are:")
+    else:
+        print(f"\n{name.title()}'s favorite language is:")
+    for language in languages:
+        print(f"\t{language.title()}")

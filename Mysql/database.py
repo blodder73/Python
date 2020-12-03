a76c8ebefbba731b30d5627c92f4966e30c77d7e
Buddy Lodder <b.lodder@bl-ict.nl>
2020-12-03 23:57:08 +0100

    New files for Mysql

---
+++ Mysql/database.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="111173bl",
+    database="testdb",
+)
+
+my_cursor = mydb.cursor()
+
+# my_cursor.execute("CREATE DATABASE testdb")
+
+# my_cursor.execute("SHOW DATABASES")
+# for db in my_cursor:
+#     print(db[0])
+
+# Create table
+# my_cursor.execute("CREATE TABLE users (name VARCHAR(255), email VARCHAR(255), age INTEGER(10), user_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
+
+my_cursor.execute("SHOW TABLES")
+
+for table in my_cursor:
+    print(table)
+
+# Create records
+sqlstuff = "INSERT INTO users (name, email, age) VALUES (%s, %s, %s)"
+
+record1 = ("John", "john@codemy.com", 40)
+
+my_cursor.execute(sqlstuff, record1)
+mydb.commit()

62a2b0b392d3dfdd1a07e2b2ba1b95644df3803b
Buddy Lodder <b.lodder@bl-ict.nl>
2020-12-10 17:55:41 +0100

    meer functies en docstrings

--- Python Masterclass/Sectie_6_Functions_An_Introduction/banner.py
+++ Python Masterclass/Sectie_6_Functions_An_Introduction/banner.py
@@ -1,4 +1,4 @@
-def banner_text(text, screen_width):
+def banner_text(text: str = " ", screen_width: int = 80) -> None:
     if len(text) > screen_width - 4:
         raise ValueError("String {0} is larger then specified width {1}".format(text, screen_width))
 
@@ -9,16 +9,16 @@ def banner_text(text, screen_width):
         print(output_string)
 
 
-banner_text("*", 50)
-banner_text("Nothing will come from nothing, ya know what they say", 80)
-banner_text("Cheer up ya old bugga c'mon give us a grin", 80)
-banner_text("There ya are, see", 80)
-banner_text("It's the end of the film", 80)
-banner_text("Incidentally this record's available in the foyer", 55)
-banner_text(" ", 80)
-banner_text("Some of us got to live as well, you know", 80)
-banner_text("(Always look on the right side of life)", 80)
-banner_text("Who do you think pays for all this rubbish", 80)
-banner_text("(Always look on the right side of life)", 80)
-banner_text("They're not gonna make their money back, you know", 80)
-banner_text("*", 33)
+banner_text("*")
+banner_text("Nothing will come from nothing, ya know what they say")
+banner_text("Cheer up ya old bugga c'mon give us a grin")
+banner_text("There ya are, see")
+banner_text("It's the end of the film")
+banner_text("Incidentally this record's available in the foyer")
+banner_text(screen_width=80)
+banner_text("Some of us got to live as well, you know")
+banner_text("(Always look on the right side of life)")
+banner_text("Who do you think pays for all this rubbish")
+banner_text("(Always look on the right side of life)")
+banner_text("They're not gonna make their money back, you know")
+banner_text("*")
--- Python Masterclass/Sectie_6_Functions_An_Introduction/functions.py
+++ Python Masterclass/Sectie_6_Functions_An_Introduction/functions.py
@@ -1,13 +1,47 @@
-def multiply(x, y):
+def multiply(x: float, y: float) -> float:
+    """
+    Get 2 integers and multiply them.
+
+    :param x: First integer
+    :param y: Second integer
+    :return: Return an `int`
+    """
     result = x * y
     return result
 
 
-def is_palindrome(string):
+def is_palindrome(string: str) -> bool:
+    """
+    Check if a string is a palindrome.
+
+    A palindrome is a string that reads the same forwards as backwards.
+
+    :param string: The string to check.
+    :return: True if `string` is a palindrome, False otherwise.
+    """
     # backwards = string[::-1]
     # return backwards == string
     return string[::-1].casefold() == string.casefold()
 
+
+def fibonacci(n: int) -> int:
+    """Return the `n` th Fibonacci number, for positive `n`."""
+    if 0 <= n <= 1:
+        return n
+
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
+for i in range(24):
+    print(i, fibonacci(i))
 
 answer = multiply(10.5, 4)
 print(answer)
